8e982ee51258e654660caebc427b0827c6a70907
Alexander Schukin <xel_s@mail.ru>
2026-05-27 18:47:37 +0300

    initial

---
+++ client/vite.config.ts
@@ -0,0 +1,21 @@
+import { defineConfig } from 'vite'
+import react, { reactCompilerPreset } from '@vitejs/plugin-react'
+import babel from '@rolldown/plugin-babel'
+
+// https://vite.dev/config/
+export default defineConfig({
+  server: {
+    proxy: {
+      "/rest": {
+        target: "http://localhost:3000",
+        changeOrigin: true,
+        secure: false,
+        rewrite: (path) => path.replace(/^\/rest/, ""),
+      },
+    },
+  },
+  plugins: [
+    react(),
+    babel({ presets: [reactCompilerPreset()] })
+  ],
+})
